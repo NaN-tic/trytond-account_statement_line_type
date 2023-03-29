0501465fb98e5b16d7ca74e202f89ee550eb9fb8
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-03-29 13:18:16 +0200

    New module "statement_line_types".
    Task #156769

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# This file is part statement_line_types module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+from . import statement
+
+def register():
+    Pool.register(
+        statement.Type,
+        statement.Line,
+        module='statement_line_types', type_='model')
---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part statement_line_types module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class StatementLineTypesTestCase(ModuleTestCase):
+    'Test Statement Line Types module'
+    module = 'statement_line_types'
+
+del ModuleTestCase
