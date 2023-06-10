caa87a3aab172263471deb21948f00496a2a2f27
Albert Cervera i Areny <albert@nan-tic.com>
2023-06-10 16:56:21 +0200

    Fix tests/__init__.py

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part statement_line_types module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_statement_line_types import suite
-
-__all__ = ['suite']
