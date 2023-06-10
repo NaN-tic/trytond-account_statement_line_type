da91356729daeaf5f4dc229c4406feab063fe5bf
Albert Cervera i Areny <albert@nan-tic.com>
2023-06-10 16:59:47 +0200

    Fix tests.

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,11 +1,10 @@
-# This file is part statement_line_types module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StatementLineTypesTestCase(ModuleTestCase):
-    'Test Statement Line Types module'
-    module = 'statement_line_types'
+class StatementLineTypeTestCase(ModuleTestCase):
+    'Test Statement Line Type module'
+    module = 'account_statement_line_type'
 
 del ModuleTestCase
